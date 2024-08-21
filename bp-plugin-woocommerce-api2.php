9c40125098dd1fa2af6de83da5b851ca2ee9a44d
Hasan Hasanzade <h.hesen@hotmail.com>
2024-08-21 12:41:20 +0400

    prepare for release

--- bp-plugin-woocommerce-api2.php
+++ bp-plugin-woocommerce-api2.php
@@ -3,7 +3,7 @@
  * Plugin Name: Better Payment WooCommerce Extension
  * Plugin URI: https://github.com/better-payment/bp-plugin-woocommerce-api2
  * Description: Better Payment plugin to implement payment methods using API2
- * Version: 1.0.0
+ * Version: 1.1.0
  * Author: Better Payment
  * Author URI: https://betterpayment.de
  * Text Domain: bp-plugin-woocommerce-api2
